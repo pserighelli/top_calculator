57f3240ff7a5225a4397d54048ac97e8f9dc29e7
pserighelli <155854304+pserighelli@users.noreply.github.com>
2024-12-16 18:01:58 +0100

    Corrects behavior when operator is clicked twice

--- calculator.js
+++ calculator.js
@@ -1,6 +1,7 @@
 let firstNum = null;
 let secondNum = null;
 let operator = null;
+let operatorClicked = false; // flag to track operator clicks
 const display = document.getElementById("display");
 const btn = document.querySelectorAll("button");
 
@@ -10,28 +11,35 @@ function clickBtn() {
             console.log(`the button clicked is ${btn[i].textContent}`)
             if (btn[i].className == "number") {
                 display.textContent = display.textContent + btn[i].textContent;
-            } if (btn[i].id == "plus") {
+            } if (btn[i].id == "plus" && !operatorClicked) {
                 firstNum = parseFloat(display.textContent);
                 operator = "+"
                 display.textContent = "";
-            } if (btn[i].id == "minus") {
+                operatorClicked = true; // prevents executing twice
+            } if (btn[i].id == "minus" && !operatorClicked) {
                 firstNum = parseFloat(display.textContent);
                 operator = "-"
                 display.textContent = "";
-            } if (btn[i].id == "multiply") {
+                operatorClicked = true;
+            } if (btn[i].id == "multiply" && !operatorClicked) {
                 firstNum = parseFloat(display.textContent);
                 operator = "*"
                 display.textContent = "";
-            } if (btn[i].id == "divide") {
+                operatorClicked = true;
+            } if (btn[i].id == "divide" && !operatorClicked) {
                 firstNum = parseFloat(display.textContent);
                 operator = "/"
                 display.textContent = "";
+                operatorClicked = true;
             } if (btn[i].id == "clear") {
                 clearAll();
             } if (btn[i].id == "equal") {
                 secondNum = parseFloat(display.textContent);
                 console.log(`the first numerator is ${firstNum}, the second numerator is ${secondNum}, and the operator is ${operator}`)
-                operate(firstNum, operator, secondNum);
+                if (operator !== null && firstNum !== null && secondNum !== null) {
+                    operate(firstNum, operator, secondNum);
+                    operatorClicked = false;
+                }
             }
         });
     }
@@ -44,11 +52,12 @@ function clearAll() {
     firstNum = null;
     secondNum = null;
     operator = null;
+    operatorClicked = false;
 }
 
 function operate(firstNum, operator, secondNum) {
-    if (operator = "/" && secondNum == 0) {
-        display.textContent == "No can do";
+    if (operator == '/' && secondNum == 0) {
+        display.textContent = "No can do";
     } else if (operator == '+') {
         display.textContent = (firstNum + secondNum);
     } else if (operator == '-') {
